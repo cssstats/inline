ce3f6c5e4ee29547f3908877de45cc613e937744
John Otander <johnotander@gmail.com>
2016-10-28 05:36:29 -0600

    Return cssstats object

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { send } = require('micro')
+const cssstats = require('cssstats')
 const getInline = require('get-inline')
 const getQueryParam = require('get-query-param')
 const normalizeUrl = require('normalize-url')
@@ -8,9 +9,11 @@ module.exports = async function (req, res) {
     const url = normalizeUrl(req.url.replace('/?url=', ''))
     const styles = await getInline(url)
 
+    const stats = cssstats(styles.map(style => `${style.tag} { ${style.style} }`).join(''))
+
     res.setHeader('Access-Control-Allow-Origin', '*')
 
-    send(res, 200, styles)
+    send(res, 200, stats)
   } catch (e) {
     send(res, 422)
   }
